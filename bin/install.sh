c8fb984890f07fa5a871f12846ee90131563f539
derrick921213 <dlin12457@gmail.com>
2021-07-09 19:36:01 +0800

    add

--- bin/install.sh
+++ bin/install.sh
@@ -17,3 +17,4 @@ fi
 curl -O https://raw.githubusercontent.com/derrick921213/Derrick_package_manager-DPM-/main/dpm.py
 pyinstaller -F dpm.py
 sudo mv dist/dpm /usr/local/bin
+sudo rm -rf __pycache__ dist build dpm.spec dpm.py
