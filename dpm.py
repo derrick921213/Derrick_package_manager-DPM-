194b4f5cea29e73b72ac4f936a7a5d79061f1472
derrick921213 <dlin12457@gmail.com>
2021-07-10 16:31:48 +0800

    add se

--- dpm.py
+++ dpm.py
@@ -146,7 +146,7 @@ def uninstall(package):
 
 def update(package=None):
     if package == None:
-        if os.system('curl -H "Cache-Control: no-cache" -O https://raw.githubusercontent.com/derrick921213/Derrick_package_manager-DPM-/main/dpm.py;pyinstaller -F dpm.py;sudo rm -rf /usr/local/bin/dpm;sudo mv dist/dpm /usr/local/bin;sudo rm -rf __pycache__ dist build dpm.spec dpm.py') == 0:
+        if os.system('export PATH="~/.local/bin:$PATH";curl -H "Cache-Control: no-cache" -O https://raw.githubusercontent.com/derrick921213/Derrick_package_manager-DPM-/main/dpm.py;pyinstaller -F dpm.py;sudo rm -rf /usr/local/bin/dpm;sudo mv dist/dpm /usr/local/bin;sudo rm -rf __pycache__ dist build dpm.spec dpm.py') == 0:
             print(f"[DPM] self update successfully!!")
         else:
             print("Plaese download again")
