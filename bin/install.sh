9044bc45c30699f6c2e502002f86720ea7c02e32
derrick921213 <dlin12457@gmail.com>
2021-07-10 16:21:45 +0800

    add

--- bin/install.sh
+++ bin/install.sh
@@ -17,5 +17,6 @@ if [ "$?" != "0" ]; then
 fi
 curl -H "Cache-Control: no-cache" -O https://raw.githubusercontent.com/derrick921213/Derrick_package_manager-DPM-/main/dpm.py
 pyinstaller -F dpm.py
+echo [DPM] Install successful.
 sudo mv dist/dpm /usr/local/bin
 sudo rm -rf __pycache__ dist build dpm.spec dpm.py
--- bin/uninstall.sh
+++ bin/uninstall.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 sudo rm -rf /usr/local/bin/dpm
+echo [DPM] Uninstall successful.
