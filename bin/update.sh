fa95c35937096f351aeb8bd767e964e8433072da
derrick921213 <dlin12457@gmail.com>
2021-07-10 23:50:13 +0800

    add

--- bin/update.sh
+++ bin/update.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 export PATH="~/.local/bin:$PATH"
+[ -f ~/.local/bin/pyinstaller ] && sudo mv ~/.local/bin/pyinstaller /usr/local/bin
 curl -H "Cache-Control: no-cache" -O https://raw.githubusercontent.com/derrick921213/Derrick_package_manager-DPM-/main/dpm.py
 pyinstaller -F dpm.py
 sudo rm -rf /usr/local/bin/dpm
