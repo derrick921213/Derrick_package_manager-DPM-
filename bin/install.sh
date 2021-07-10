c968e9ea948a8d3c1a5e68cf64e3999e3ab577c8
derrick921213 <dlin12457@gmail.com>
2021-07-10 16:14:25 +0800

    add

--- bin/install.sh
+++ bin/install.sh
@@ -14,7 +14,7 @@ if [ "$?" != "0" ]; then
         exit 1
     fi
 fi
-curl -O https://raw.githubusercontent.com/derrick921213/Derrick_package_manager-DPM-/main/dpm.py
+curl -H "Cache-Control: no-cache" -O https://raw.githubusercontent.com/derrick921213/Derrick_package_manager-DPM-/main/dpm.py
 pyinstaller -F dpm.py
 sudo mv dist/dpm /usr/local/bin
 sudo rm -rf __pycache__ dist build dpm.spec dpm.py
