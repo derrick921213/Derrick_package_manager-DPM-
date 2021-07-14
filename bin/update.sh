37e5f062d4e019a32079e2ed8e45b6cc7536c20b
derrick921213 <dlin12457@gmail.com>
2021-07-14 10:37:54 +0800

    add

--- bin/update.sh
+++ bin/update.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 export PATH="~/.local/bin:$PATH"
+[ ! -d /usr/local/DPM ] && echo "DPM is not install.See my github to install."
 [ -f ~/.local/bin/pyinstaller ] && sudo mv ~/.local/bin/pyinstaller /usr/local/bin
 curl -H "Cache-Control: no-cache" -o dpm.py https://raw.githubusercontent.com/derrick921213/Derrick_package_manager-DPM-/main/dpm.py?$(date +%s)
 pyinstaller -F dpm.py
