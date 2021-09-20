0cfccb1e52600e141fae7906b63075d0ea85f3bd
derrick921213 <dlin12457@gmail.com>
2021-09-20 14:14:48 +0800

    fix

--- dpm.py
+++ dpm.py
@@ -21,62 +21,130 @@ class system_shell:
     def linux_shell(self, package, install=False, uninstall=False, update=False, verbose=False):
         if install is True and uninstall is False and update is False:
             if os.system("which apt >/dev/null") == 0:
-                if os.system(f"sudo apt install {package} -y") == 0:
+                if verbose:
+                    if os.system(f"sudo apt install {package} -y") == 0:
+                        print(f'[{package}] Install from apt')
+                        sys.exit(0)
+                if os.system(f"sudo apt install {package} -y 1>/dev/null") == 0:
                     print(f'[{package}] Install from apt')
+                    sys.exit(0)
             elif os.system("which dnf >/dev/null") == 0:
-                if os.system(f"sudo dnf install {package} -y") == 0:
+                if verbose:
+                    if os.system(f"sudo dnf install {package} -y") == 0:
+                        print(f'[{package}] Install from dnf')
+                        sys.exit(0)
+                if os.system(f"sudo dnf install {package} -y 1>/dev/null") == 0:
                     print(f'[{package}] Install from dnf')
+                    sys.exit(0)
             elif os.system("which yum >/dev/null") == 0:
-                if os.system(f"sudo yum install {package} -y") == 0:
+                if verbose:
+                    if os.system(f"sudo yum install {package} -y") == 0:
+                        print(f'[{package}] Install from yum')
+                        sys.exit(0)
+                if os.system(f"sudo yum install {package} -y 1>/dev/null") == 0:
                     print(f'[{package}] Install from yum')
+                    sys.exit(0)
             else:
                 print('Package manager not found')
+                sys.exit(1)
         elif install is False and uninstall is True and update is False:
             if os.system("which apt >/dev/null") == 0:
-                if os.system(f"sudo apt remove {package} -y") == 0:
+                if verbose:
+                    if os.system(f"sudo apt remove {package} -y") == 0:
+                        print(f'[{package}] Uninstall from apt')
+                        sys.exit(0)
+                if os.system(f"sudo apt remove {package} -y 1>/dev/null") == 0:
                     print(f'[{package}] Uninstall from apt')
+                    sys.exit(0)
             elif os.system("which dnf >/dev/null") == 0:
-                if os.system(f"sudo dnf install {package} -y") == 0:
+                if verbose:
+                    if os.system(f"sudo dnf install {package} -y ") == 0:
+                        print(f'[{package}] Uninstall from dnf')
+                        sys.exit(0)
+                if os.system(f"sudo dnf install {package} -y 1>/dev/null") == 0:
                     print(f'[{package}] Uninstall from dnf')
+                    sys.exit(0)
             elif os.system("which yum >/dev/null") == 0:
-                if os.system(f"sudo yum remove {package} -y") == 0:
+                if verbose:
+                    if os.system(f"sudo yum remove {package} -y ") == 0:
+                        print(f'[{package}] Uninstall from yum')
+                        sys.exit(0)
+                if os.system(f"sudo yum remove {package} -y 1>/dev/null") == 0:
                     print(f'[{package}] Uninstall from yum')
+                    sys.exit(0)
             else:
                 print('Package manager not found')
+                sys.exit(1)
         elif install is False and uninstall is False and update is True:
             if os.system("which apt >/dev/null") == 0:
-                if os.system(f"sudo apt update {package} -y") == 0:
+                if verbose:
+                    if os.system(f"sudo apt update {package} -y") == 0:
+                        print(f'[{package}] update from apt')
+                        sys.exit(0)
+                if os.system(f"sudo apt update {package} -y 1>/dev/null") == 0:
                     print(f'[{package}] update from apt')
+                    sys.exit(0)
             elif os.system("which dnf >/dev/null") == 0:
-                if os.system(f"sudo dnf update {package} -y") == 0:
+                if verbose:
+                    if os.system(f"sudo dnf update {package} -y") == 0:
+                        print(f'[{package}] update from dnf')
+                        sys.exit(0)
+                if os.system(f"sudo dnf update {package} -y 1>/dev/null") == 0:
                     print(f'[{package}] update from dnf')
+                    sys.exit(0)
             elif os.system("which yum >/dev/null") == 0:
-                if os.system(f"sudo yum update {package} -y") == 0:
+                if verbose:
+                    if os.system(f"sudo yum update {package} -y") == 0:
+                        print(f'[{package}] update from yum')
+                        sys.exit(0)
+                if os.system(f"sudo yum update {package} -y 1>/dev/null") == 0:
                     print(f'[{package}] update from yum')
+                    sys.exit(0)
             else:
                 print('Package manager not found')
+                sys.exit(1)
         else:
             print("Application Error")
+            sys.exit(1)
 
     def mac_shell(self, package, install=False, uninstall=False, update=False, verbose=False):
         if install is True and uninstall is False and update is False:
             if os.system("which brew >/dev/null") == 0:
-                if os.system(f"brew install {package}") == 0:
+                if verbose:
+                    if os.system(f"brew install {package}") == 0:
+                        print(f'[{package}] Install from Homebrew')
+                        sys.exit(0)
+                if os.system(f"brew install {package} 1>/dev/null") == 0:
                     print(f'[{package}] Install from Homebrew')
+                    sys.exit(0)
             else:
                 print('Homebrew not found')
+                sys.exit(1)
         elif install is False and uninstall is True and update is False:
-            if os.system(f"brew uninstall {package}") == 0:
+            if verbose:
+                if os.system(f"brew uninstall {package}") == 0:
+                    print(f'[{package}] was Removed from Homebrew')
+                    sys.exit(0)
+            if os.system(f"brew uninstall {package} 1>/dev/null") == 0:
                 print(f'[{package}] was Removed from Homebrew')
+                sys.exit(0)
             else:
                 print(f"Remove [{package}] Error!!")
+                sys.exit(1)
         elif install is False and update is False and update is True:
-            if os.system(f"brew upgrade {package}") == 0:
+            if verbose:
+                if os.system(f"brew upgrade {package}") == 0:
+                    print(f'[{package}] was Upgraded from Homebrew')
+                    sys.exit(0)
+            if os.system(f"brew upgrade {package} 1>/dev/null") == 0:
                 print(f'[{package}] was Upgraded from Homebrew')
+                sys.exit(0)
             else:
                 print(f"Upgrade [{package}] Error!!")
+                sys.exit(1)
         else:
             print("Application Error")
+            sys.exit(1)
 
 
 class package_download:
@@ -127,28 +195,49 @@ class package_download:
 
 
 class Action:
-    def install_update(self, package):
+    def install_update(self, package, verbose=False):
+        if verbose:
+            if os.path.isdir(f"/usr/local/DPM/{package}") and os.path.isfile(f"/usr/local/bin/{package}"):
+                error = os.system(
+                    f'sudo rm -rf /usr/local/bin/{package} /usr/local/DPM/{package}')
+                if error == 0:
+                    local = os.system(
+                        f"sudo mkdir -p /usr/local/DPM/{package};sudo chown -R $USER /usr/local/DPM/*")
+                    if local == 0:
+                        file = os.system(f"cd /tmp;ls | grep '^dpm_{package}'")
+                        if file == 0:
+                            download = package_download()
+                            info = download.read_package_info(package)
+                            os.system(
+                                f"temp=/tmp;a=`ls $temp | grep '^dpm_{package}'`;tar -xf $temp/$a -C /usr/local/DPM/{package};sudo chmod -R 555 /usr/local/DPM/*;sudo ln -s /usr/local/DPM/{package}/{info['main_file']} /usr/local/bin/{package};rm $temp/$a")
+                        else:
+                            print('Package NO Found')
+                            sys.exit(1)
+                else:
+                    print('Package Error')
+                    sys.exit(1)
         if os.path.isdir(f"/usr/local/DPM/{package}") and os.path.isfile(f"/usr/local/bin/{package}"):
             error = os.system(
-                f'sudo rm -rf /usr/local/bin/{package} /usr/local/DPM/{package}')
+                f'sudo rm -rf /usr/local/bin/{package} /usr/local/DPM/{package} >/dev/null 2>&1')
             if error == 0:
                 local = os.system(
-                    f"sudo mkdir -p /usr/local/DPM/{package};sudo chown -R $USER /usr/local/DPM/*")
+                    f"sudo mkdir -p /usr/local/DPM/{package};sudo chown -R $USER /usr/local/DPM/* >/dev/null 2>&1")
                 if local == 0:
-                    file = os.system(f"cd /tmp;ls | grep '^dpm_{package}'")
+                    file = os.system(
+                        f"cd /tmp;ls | grep '^dpm_{package}' >/dev/null 2>&1")
                     if file == 0:
                         download = package_download()
                         info = download.read_package_info(package)
                         os.system(
-                            f"temp=/tmp;a=`ls $temp | grep '^dpm_{package}'`;tar -xf $temp/$a -C /usr/local/DPM/{package};sudo chmod -R 555 /usr/local/DPM/*;sudo ln -s /usr/local/DPM/{package}/{info['main_file']} /usr/local/bin/{package};rm $temp/$a")
+                            f"temp=/tmp;a=`ls $temp | grep '^dpm_{package}'`&&tar -xf $temp/$a -C /usr/local/DPM/{package}&&sudo chmod -R 555 /usr/local/DPM/*&&sudo ln -s /usr/local/DPM/{package}/{info['main_file']} /usr/local/bin/{package}&&rm $temp/$a >/dev/null 2>&1")
                     else:
                         print('Package NO Found')
                         sys.exit(1)
-            else:
-                print('Package Error')
-                sys.exit(1)
+                else:
+                    print('Package Error')
+                    sys.exit(1)
 
-    def install(self, package):
+    def install(self, package, verbose=False):
         download = package_download()
         is_my = download.package_list()
         if package in is_my:
@@ -157,14 +246,38 @@ class Action:
             local = os.system(
                 f"sudo mkdir -p /usr/local/DPM/{package};sudo chown -R $USER /usr/local/DPM/*")
             if local == 0:
-                file = os.system(f"cd /tmp;ls | grep '^dpm_{package}'")
-                if file == 0:
-                    info = download.read_package_info(package)
-                    os.system(
-                        f"temp=/tmp;a=`ls $temp | grep '^dpm_{package}'`;tar -xf $temp/$a -C /usr/local/DPM/{package};sudo chmod -R 555 /usr/local/DPM/*;sudo ln -s /usr/local/DPM/{package}/{info['main_file']} /usr/local/bin/{package};rm $temp/$a")
+                if verbose:
+                    file = os.system(
+                        f"cd /tmp;ls | grep '^dpm_{package}'")
+                    if file == 0:
+                        info = download.read_package_info(package)
+                        test = os.system(
+                            f"temp=/tmp;a=`ls $temp | grep '^dpm_{package}'`&&tar -xf $temp/$a -C /usr/local/DPM/{package}&&sudo chmod -R 555 /usr/local/DPM/*&&sudo ln -s /usr/local/DPM/{package}/{info['main_file']} /usr/local/bin/{package}&&rm $temp/$a")
+                        if test == 0:
+                            print(f'{package} install Success')
+                            sys.exit(0)
+                        else:
+                            print(f'{package} install Fail')
+                            sys.exit(1)
+                    else:
+                        print('Package NOT FOUND')
+                        sys.exit(1)
                 else:
-                    print('Package NO Found')
-                    sys.exit(1)
+                    file = os.system(
+                        f"cd /tmp;ls | grep '^dpm_{package}'>/dev/null")
+                    if file == 0:
+                        info = download.read_package_info(package)
+                        test1 = os.system(
+                            f"temp=/tmp;a=`ls $temp | grep '^dpm_{package}'`>/dev/null 2>&1&&tar -xf $temp/$a -C /usr/local/DPM/{package}>/dev/null 2>&1&&sudo chmod -R 555 /usr/local/DPM/*>/dev/null 2>&1&&sudo ln -s /usr/local/DPM/{package}/{info['main_file']} /usr/local/bin/{package}>/dev/null 2>&1&&rm $temp/$a >/dev/null 2>&1")
+                        if test1 == 0:
+                            print(f'{package} install Success')
+                            sys.exit(0)
+                        else:
+                            print(f'{package} install Fail')
+                            sys.exit(1)
+                    else:
+                        print('Package NOT FOUND')
+                        sys.exit(1)
             else:
                 print('Package Error')
                 sys.exit(1)
@@ -172,17 +285,29 @@ class Action:
             shell = system_shell()
             system = shell.system_platform()
             if system == 'linux':
+                if verbose:
+                    shell.linux_shell(package, install=True, verbose=True)
                 shell.linux_shell(package, install=True)
             elif system == 'darwin':
+                if verbose:
+                    shell.mac_shell(package, install=True, verbose=True)
                 shell.mac_shell(package, install=True)
 
-    def uninstall(self, package):
+    def uninstall(self, package, verbose=False):
         download = package_download()
         is_my = download.package_list()
         if package in is_my:
             if len(download.installed_package_list()) > 0:
-                if os.path.isdir(f"/usr/local/DPM/{package}"):
-                    if os.system(f"sudo rm -rf /usr/local/DPM/{package} /usr/local/bin/{package}") == 0:
+                if verbose:
+                    if os.path.isdir(f"/usr/local/DPM/{package}"):
+                        if os.system(f"sudo rm -rf /usr/local/DPM/{package} /usr/local/bin/{package}") == 0:
+                            print(f"[{package}] Removed!!")
+                        else:
+                            print(f"Remove [{package}] Error!!")
+                    else:
+                        print(f'[{package}] not installed')
+                if os.path.isdir(f"/usr/local/DPM/{package} >/dev/null 2>&1"):
+                    if os.system(f"sudo rm -rf /usr/local/DPM/{package} /usr/local/bin/{package} >/dev/null 2>&1") == 0:
                         print(f"[{package}] Removed!!")
                     else:
                         print(f"Remove [{package}] Error!!")
@@ -192,14 +317,27 @@ class Action:
             shell = system_shell()
             system = shell.system_platform()
             if system == 'linux':
+                if verbose:
+                    shell.linux_shell(package, uninstall=True, verbose=True)
                 shell.linux_shell(package, uninstall=True)
             elif system == 'darwin':
+                if verbose:
+                    shell.mac_shell(package, uninstall=True, verbose=True)
                 shell.mac_shell(package, uninstall=True)
 
-    def update(self, package=None):
+    def update(self, package=None, verbose=False):
         if package is None:
-            os.system('/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/derrick921213/Derrick_package_manager-DPM-/main/bin/update.sh?$(date +%s))"')
-            print(f"[DPM] self update successfully!!")
+            if verbose:
+                test = os.system(
+                    '/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/derrick921213/Derrick_package_manager-DPM-/main/bin/update.sh?$(date +%s))"')
+                if test == 0:
+                    print(f"[DPM] self update successfully!!")
+                    sys.exit(0)
+            test2 = os.system(
+                '/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/derrick921213/Derrick_package_manager-DPM-/main/bin/update.sh?$(date +%s))" >/dev/null 2>&1')
+            if test2 == 0:
+                print(f"[DPM] self update successfully!!")
+                sys.exit(0)
         else:
             download = package_download()
             is_my = download.package_list()
@@ -214,7 +352,7 @@ class Action:
                                     download.read_package_list(package))
                                 info = download.read_package_info(package)
                                 if info["version"] > installed_info["version"]:
-                                    self.install_update(package)
+                                    self.install_update(package, verbose)
                                 else:
                                     print(f"[{package}] no update!!")
                         else:
@@ -227,19 +365,13 @@ class Action:
                 shell = system_shell()
                 system = shell.system_platform()
                 if system == 'linux':
+                    if verbose:
+                        shell.linux_shell(package, update=True, verbose=True)
                     shell.linux_shell(package, update=True)
                 elif system == 'darwin':
+                    if verbose:
+                        shell.mac_shell(package, update=True, verbose=True)
                     shell.mac_shell(package, update=True)
-
-    def help(self):
-        print('''[DPM] commands
-    dpm install   ----Install package
-    dpm search    ----Search package
-    dpm uninstall ----Uninstall package
-    dpm list      ----List installed package
-    dpm help      ----This help page
-    dpm update    ----Update package
-    ''')
 
 
 class main:
@@ -250,9 +382,9 @@ class main:
         act = Action()
         _package = " ".join(package)
         if args == "install":
-            act.install(_package)
+            act.install(_package, verbose)
         elif args == "uninstall":
-            act.uninstall(_package)
+            act.uninstall(_package, verbose)
         elif args == "list":
             package_download().installed_package_list()
         elif args == "search":
@@ -266,7 +398,7 @@ class main:
             else:
                 package_download().read_package_list(_package)
         elif args == "update":
-            act.update(_package)
+            act.update(_package, verbose)
 
 
 if __name__ == '__main__':
